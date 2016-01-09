167b549c4710cf69c2e60f48560d1f70dcdfaecf
Tim Reidel <reideltj@yahoo.com>
2016-01-08 23:38:21 -0500

    bug fix

--- power_records_to_hourly/power_records_to_hourly.py
+++ power_records_to_hourly/power_records_to_hourly.py
@@ -39,7 +39,7 @@ def lambda_handler (event, context):
         # create the attribute updates
         attribute_updates = {}
         for circuit_index, measurement in message['measurements'].iteritems():
-            energy_in_kwh = Decimal(str(circuit['energy-in-kwh']))
+            energy_in_kwh = Decimal(str(measurement['energy-in-kwh']))
             attribute_update = {'Action' : 'ADD', 'Value' : energy_in_kwh}
             attribute_updates['energy-in-kwh-{}'.format(circuit_index)] = attribute_update
             
